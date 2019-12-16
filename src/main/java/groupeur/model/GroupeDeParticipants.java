c797ae657d8930e48485a08e4df2d3ee5e9e6ea5
PINEL Vincent <vpinel@softwaymedical.fr>
2019-12-16 14:15:45 +0100

    feat: debut dev

--- src/main/java/groupeur/model/GroupeDeParticipants.java
+++ src/main/java/groupeur/model/GroupeDeParticipants.java
@@ -1,5 +1,19 @@
 package groupeur.model;
 
+import static com.google.common.collect.Sets.newHashSet;
+
+import java.util.Set;
+
 public class GroupeDeParticipants {
+
+	private final Set<Participant> participants;
+	
+	public GroupeDeParticipants() {
+		this.participants = newHashSet();
+	}
+
+	public void ajoute(Participant unParticipant) {
+		this.participants.add(unParticipant);
+	}
 
 }
