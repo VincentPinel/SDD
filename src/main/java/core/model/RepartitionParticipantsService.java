f681bd59d5c6f2b43c851cc409c354b487a15815
PINEL Vincent <vpinel@softwaymedical.fr>
2019-12-16 14:15:45 +0100

    feat: debut dev

--- src/main/java/core/model/RepartitionParticipantsService.java
+++ src/main/java/core/model/RepartitionParticipantsService.java
@@ -3,18 +3,20 @@ package core.model;
 import java.util.Set;
 
 import afficheur.service.AfficheurService;
+import groupeur.model.CreateurDeGroupes;
 import groupeur.model.GroupeDeParticipants;
 import groupeur.model.Participant;
-import groupeur.service.GroupeurService;
+import groupeur.service.Groupeur;
 import parser.service.FichierParticipantsParserService;
 
 public class RepartitionParticipantsService {
 
-	public void repartit() {
+	public void repartit(int tailleDesGroupes) {
 		FichierParticipantsParserService parser = new FichierParticipantsParserService();
 		Set<Participant> participants = parser.donneParticipants();
-		Set<GroupeDeParticipants> groupes = new GroupeurService(participants).groupe();
-		new AfficheurService(groupes).affiche();
+		Set<GroupeDeParticipants> groupesVides = new CreateurDeGroupes(participants.size(), tailleDesGroupes).creeGroupes();
+		Set<GroupeDeParticipants> groupesFormes = new Groupeur().groupe(participants, groupesVides);
+		new AfficheurService(groupesFormes).affiche();
 	}
 
 }
